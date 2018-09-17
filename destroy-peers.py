rockon1985/hyperledger-fabric-on-kubernetes

Adds Helm charts and python scsript to setup K8s Cluster

destroy-peers.py
@@ -1,0 +1,34 @@
+#!/usr/bin/env python
+import yaml
+import os
+import sys
+
+print("""\033[93m
+    | |                            
+  __| | __ _ _ __   __ _  ___ _ __ 
+ / _` |/ _` | '_ \ / _` |/ _ \ '__|
+| (_| | (_| | | | | (_| |  __/ |   
+ \__,_|\__,_|_| |_|\__, |\___|_|   
+                    __/ |          
+                   |___/           
+""")
+print("NOTE: This operation will remove the existing fabric peer cluster \033[0m")
+res = raw_input("Do you wish to continue (y/n):")
+if res != 'y':
+  sys.exit(0)
+
+with open("./crypto-config.yaml", 'r') as stream:
+  try:
+    config = yaml.load(stream)
+    os.system('helm del --purge public-certs-pvc')
+    for org in config['PeerOrgs']:
+      namespace = org['Name'].lower()
+      os.system("helm del --purge %s-geotrade-node" %namespace)
+      os.system("helm del --purge cli-%s-pvc" %namespace)
+      os.system("helm del --purge cli-%s" %namespace)
+      for p in org['Specs']:
+        os.system("helm del --purge pvc-%s" %p['CommonName'])
+        os.system("helm del --purge %s" %p['CommonName'])
+      
+  except yaml.YAMLError as exc:
+    print(exc)

org-peer/templates/_helpers.tpl
@@ -1,0 +1,55 @@
+{{/* vim: set filetype=mustache: */}}
+{{/*
+Expand the name of the chart.
+*/}}
+{{- define "geoverse-ecosystem.name" -}}
+{{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+
+{{/*
+Create a default fully qualified app name.
+We truncate at 63 chars because some Kubernetes name fields are limited to this (by the DNS naming spec).
+If release name contains chart name it will be used as a full name.
+*/}}
+{{- define "geoverse-ecosystem.fullname" -}}
+{{- if .Values.fullnameOverride -}}
+{{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" -}}
+{{- else -}}
+{{- $name := default .Chart.Name .Values.nameOverride -}}
+{{- if contains $name .Release.Name -}}
+{{- .Release.Name | trunc 63 | trimSuffix "-" -}}
+{{- else -}}
+{{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+{{- end -}}
+{{- end -}}
+
+{{/*
+Create chart name and version as used by the chart label.
+*/}}
+{{- define "geoverse-ecosystem.chart" -}}
+{{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+
+{{- define "hlf-peer.name" -}}
+{{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+
+{{- define "hlf-peer.fullname" -}}
+{{- if .Values.fullnameOverride -}}
+{{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" -}}
+{{- else -}}
+{{- $name := default .Chart.Name .Values.nameOverride -}}
+{{- if contains $name .Release.Name -}}
+{{- .Release.Name | trunc 63 | trimSuffix "-" -}}
+{{- else -}}
+{{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+{{- end -}}
+{{- end -}}
+{{- define "labels.standard" -}}
+app: {{ include "hlf-peer.name" . }}
+heritage: {{ .Release.Service | quote }}
+release: {{ .Release.Name | quote }}
+chart: {{ include "hlf-peer.chart" . }}
+{{- end -}}

scripts/script.sh
@@ -1,0 +1,117 @@
+#!/bin/bash
+
+echo
+echo " ____    _____      _      ____    _____ "
+echo "/ ___|  |_   _|    / \    |  _ \  |_   _|"
+echo "\___ \    | |     / _ \   | |_) |   | |  "
+echo " ___) |   | |    / ___ \  |  _ <    | |  "
+echo "|____/    |_|   /_/   \_\ |_| \_\   |_|  "
+echo
+echo
+CHANNEL_NAME="$1"
+DELAY="$2"
+LANGUAGE="$3"
+TIMEOUT="$4"
+VERBOSE="$5"
+: ${CHANNEL_NAME:="mychannel"}
+: ${DELAY:="3"}
+: ${LANGUAGE:="golang"}
+: ${TIMEOUT:="10"}
+: ${VERBOSE:="false"}
+LANGUAGE=`echo "$LANGUAGE" | tr [:upper:] [:lower:]`
+COUNTER=1
+MAX_RETRY=5
+
+
+# CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/sla_chaincode/node/"
+CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/default_chaincode/node/"
+
+echo "Channel name : "$CHANNEL_NAME
+
+# import utils
+. scripts/utils.sh
+
+createChannel() {
+	setGlobals 0 1
+
+	if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+                set -x
+		peer channel create -o orderer.geoverse.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx >&log.txt
+		res=$?
+                set +x
+	else
+				set -x
+		peer channel create -o orderer.geoverse.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA >&log.txt
+		res=$?
+				set +x
+	fi
+	cat log.txt
+	verifyResult $res "Channel creation failed"
+	echo "===================== Channel '$CHANNEL_NAME' created ===================== "
+	echo
+}
+
+joinChannel () {
+	for org in 1 2; do
+	    for peer in 0 1; do
+		joinChannelWithRetry $peer $org
+		echo "===================== peer${peer}.org${org} joined channel '$CHANNEL_NAME' ===================== "
+		sleep $DELAY
+		echo
+	    done
+	done
+}
+
+## Create channel
+echo "Creating channel..."
+createChannel
+
+## Join all the peers to the channel
+echo "Having all peers join the channel..."
+joinChannel
+
+## Set the anchor peers for each org in the channel
+echo "Updating anchor peers for org1..."
+updateAnchorPeers 0 1
+echo "Updating anchor peers for org2..."
+updateAnchorPeers 0 2
+
+## Install chaincode on peer0.org1 and peer0.org2
+echo "Installing chaincode on peer0.org1..."
+installChaincode 0 1
+echo "Install chaincode on peer0.org2..."
+installChaincode 0 2
+
+# Instantiate chaincode on peer0.org2
+echo "Instantiating chaincode on peer0.org2..."
+instantiateChaincode 0 2
+
+# Query chaincode on peer0.org1
+# echo "Querying chaincode on peer0.org1..."
+# chaincodeQuery 0 1 100
+
+# Invoke chaincode on peer0.org1 and peer0.org2
+# echo "Sending invoke transaction on peer0.org1 peer0.org2..."
+# chaincodeInvoke 0 1 0 2
+
+## Install chaincode on peer1.org2
+echo "Installing chaincode on peer1.org2..."
+installChaincode 1 2
+
+# Query on chaincode on peer1.org2, check if the result is 90
+# echo "Querying chaincode on peer1.org2..."
+# chaincodeQuery 1 2 90
+
+echo
+echo "========= All GOOD, execution completed =========== "
+echo
+
+echo
+echo " _____   _   _   ____   "
+echo "| ____| | \ | | |  _ \  "
+echo "|  _|   |  \| | | | | | "
+echo "| |___  | |\  | | |_| | "
+echo "|_____| |_| \_| |____/  "
+echo
+
+exit 0

scripts/utils.sh
@@ -1,0 +1,194 @@
+ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/geoverse.com/orderers/orderer.geoverse.com/msp/tlscacerts/tlsca.geoverse.com-cert.pem
+PEER0_ORG1_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/buyerone.geoverse.com/peers/peer0.buyerone.geoverse.com/tls/ca.crt
+PEER0_ORG2_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/sellerone.geoverse.com/peers/peer0.sellerone.geoverse.com/tls/ca.crt
+
+# verify the result of the end-to-end test
+verifyResult() {
+  if [ $1 -ne 0 ]; then
+    echo "!!!!!!!!!!!!!!! "$2" !!!!!!!!!!!!!!!!"
+    echo "========= ERROR !!! FAILED to execute End-2-End Scenario ==========="
+    echo
+    exit 1
+  fi
+}
+
+# Set OrdererOrg.Admin globals
+setOrdererGlobals() {
+  CORE_PEER_LOCALMSPID="OrdererMSP"
+  CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/geoverse.com/orderers/orderer.geoverse.com/msp/tlscacerts/tlsca.geoverse.com-cert.pem
+  CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/geoverse.com/users/Admin@geoverse.com/msp
+}
+
+setGlobals() {
+  PEER=$1
+  ORG=$2
+  if [ $ORG -eq 1 ]; then
+    CORE_PEER_LOCALMSPID="BuyerOneMSP"
+    CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+    CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/buyerone.geoverse.com/users/Admin@buyerone.geoverse.com/msp
+    if [ $PEER -eq 0 ]; then
+      CORE_PEER_ADDRESS=peer0.buyerone.geoverse.com:7051
+    else
+      CORE_PEER_ADDRESS=peer1.buyerone.geoverse.com:7051
+    fi
+  elif [ $ORG -eq 2 ]; then
+    CORE_PEER_LOCALMSPID="SellerOneMSP"
+    CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
+    CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/sellerone.geoverse.com/users/Admin@sellerone.geoverse.com/msp
+    if [ $PEER -eq 0 ]; then
+      CORE_PEER_ADDRESS=peer0.sellerone.geoverse.com:7051
+    else
+      CORE_PEER_ADDRESS=peer1.sellerone.geoverse.com:7051
+    fi
+  else
+    echo "================== ERROR !!! ORG Unknown =================="
+  fi
+
+  if [ "$VERBOSE" == "true" ]; then
+    env | grep CORE
+  fi
+}
+
+updateAnchorPeers() {
+  PEER=$1
+  ORG=$2
+  setGlobals $PEER $ORG
+
+  if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+    set -x
+    peer channel update -o orderer.geoverse.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx >&log.txt
+    res=$?
+    set +x
+  else
+    set -x
+    peer channel update -o orderer.geoverse.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA >&log.txt
+    res=$?
+    set +x
+  fi
+  cat log.txt
+  verifyResult $res "Anchor peer update failed"
+  echo "===================== Anchor peers updated for org '$CORE_PEER_LOCALMSPID' on channel '$CHANNEL_NAME' ===================== "
+  sleep $DELAY
+  echo
+}
+
+## Sometimes Join takes time hence RETRY at least 5 times
+joinChannelWithRetry() {
+  PEER=$1
+  ORG=$2
+  setGlobals $PEER $ORG
+
+  set -x
+  peer channel join -b $CHANNEL_NAME.block >&log.txt
+  res=$?
+  set +x
+  cat log.txt
+  if [ $res -ne 0 -a $COUNTER -lt $MAX_RETRY ]; then
+    COUNTER=$(expr $COUNTER + 1)
+    echo "peer${PEER}.org${ORG} failed to join the channel, Retry after $DELAY seconds"
+    sleep $DELAY
+    joinChannelWithRetry $PEER $ORG
+  else
+    COUNTER=1
+  fi
+  verifyResult $res "After $MAX_RETRY attempts, peer${PEER}.org${ORG} has failed to join channel '$CHANNEL_NAME' "
+}
+
+installChaincode() {
+  PEER=$1
+  ORG=$2
+  setGlobals $PEER $ORG
+  VERSION=${3:-1.0}
+  set -x
+  peer chaincode install -n mycc -v ${VERSION} -l ${LANGUAGE} -p ${CC_SRC_PATH} >&log.txt
+  res=$?
+  set +x
+  cat log.txt
+  verifyResult $res "Chaincode installation on peer${PEER}.org${ORG} has failed"
+  echo "===================== Chaincode is installed on peer${PEER}.org${ORG} ===================== "
+  echo
+}
+
+instantiateChaincode() {
+  PEER=$1
+  ORG=$2
+  setGlobals $PEER $ORG
+  VERSION=${3:-1.0}
+
+  # while 'peer chaincode' command can get the orderer endpoint from the peer
+  # (if join was successful), let's supply it directly as we know it using
+  # the "-o" option
+  if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+    set -x
+    peer chaincode instantiate -o orderer.geoverse.com:7050 -C $CHANNEL_NAME -n mycc -l ${LANGUAGE} -v ${VERSION} -c '{"Args":["init","key","100"]}' -P "OR ('BuyerOneMSP.peer','SellerOneMSP.peer')" >&log.txt
+    res=$?
+    set +x
+  else
+    set -x
+    # peer chaincode instantiate -o orderer.geoverse.com:7050 --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -C $CHANNEL_NAME -n mycc -l ${LANGUAGE} -v 1.0 -c '{"Args":["init", "{\"locations\":{\"longitude\":\"10.10\",\"latitude\":\"10.10\",\"altitude\":\"10.10\"},\"technology\":\"LTE\",\"technologyOptions\":\"Rel 13\",\"qualityOfService\":{\"totalAvailableBandwidth\":\"10\",\"peakUplinkSpeed\":\"20\"},\"accessControlType\":\"PLMN\",\"userIdentity\":\"user code of buyers users\",\"routingType\":\"LB\",\"metricOfValue\":{\"IMSI\":\"91\"},\"timePeriod\":{\"from\":\"some epoch time\",\"to\":\"some other epoch time > from\"},\"price\":{\"perGB\":\"10\"},\"pricingType\":\"one time\",\"recurringFrequency\":\"daily\",\"onetimeDuratrion\":\"1 year\",\"paymentTerms\":\"up-front\",\"prepaidAmount\":\"100\",\"topup amount\":\"100\",\"renewals\":\"5 days\",\"contractSeller\":\"seller ID\",\"contractBuyer\":\"buyer ID\",\"contractStatus\":\"inactive\",\"performanceReporting\":[\"number of unique users\",\"square feet utilized\"],\"reportingTolerance\":\"10\",\"reportingFrequency\":\"monthly\"}"]}' -P "OR ('BuyerOneMSP.peer','SellerOneMSP.peer')" >&log.txt
+    # 
+    # 
+    peer chaincode instantiate -o orderer.geoverse.com:7050 --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -C $CHANNEL_NAME -n mycc -l ${LANGUAGE} -v 1.0 -c '{"Args":["init","key","jayesh"]}' -P "OR ('BuyerOneMSP.peer','SellerOneMSP.peer')" >&log.txt
+    res=$?
+    set +x
+  fi
+  cat log.txt
+  verifyResult $res "Chaincode instantiation on peer${PEER}.org${ORG} on channel '$CHANNEL_NAME' failed"
+  echo "===================== Chaincode is instantiated on peer${PEER}.org${ORG} on channel '$CHANNEL_NAME' ===================== "
+  echo
+}
+
+# fetchChannelConfig <channel_id> <output_json>
+# Writes the current channel config for a given channel to a JSON file
+fetchChannelConfig() {
+  CHANNEL=$1
+  OUTPUT=$2
+
+  setOrdererGlobals
+
+  echo "Fetching the most recent configuration block for the channel"
+  if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+    set -x
+    peer channel fetch config config_block.pb -o orderer.geoverse.com:7050 -c $CHANNEL --cafile $ORDERER_CA
+    set +x
+  else
+    set -x
+    peer channel fetch config config_block.pb -o orderer.geoverse.com:7050 -c $CHANNEL --tls --cafile $ORDERER_CA
+    set +x
+  fi
+
+  echo "Decoding config block to JSON and isolating config to ${OUTPUT}"
+  set -x
+  configtxlator proto_decode --input config_block.pb --type common.Block | jq .data.data[0].payload.data.config >"${OUTPUT}"
+  set +x
+}
+
+# signConfigtxAsPeerOrg <org> <configtx.pb>
+# Set the peerOrg admin of an org and signing the config update
+signConfigtxAsPeerOrg() {
+  PEERORG=$1
+  TX=$2
+  setGlobals 0 $PEERORG
+  set -x
+  peer channel signconfigtx -f "${TX}"
+  set +x
+}
+
+# createConfigUpdate <channel_id> <original_config.json> <modified_config.json> <output.pb>
+# Takes an original and modified config, and produces the config update tx
+# which transitions between the two
+createConfigUpdate() {
+  CHANNEL=$1
+  ORIGINAL=$2
+  MODIFIED=$3
+  OUTPUT=$4
+
+  set -x
+  configtxlator proto_encode --input "${ORIGINAL}" --type common.Config >original_config.pb
+  configtxlator proto_encode --input "${MODIFIED}" --type common.Config >modified_config.pb
+  configtxlator compute_update --channel_id "${CHANNEL}" --original original_config.pb --updated modified_config.pb >config_update.pb
+  configtxlator proto_decode --input config_update.pb --type common.ConfigUpdate >config_update.json
+  echo '{"payload":{"header":{"channel_header":{"channel_id":"'$CHANNEL'", "type":2}},"data":{"config_update":'$(cat config_update.json)'}}}' | jq . >config_update_in_envelope.json
+  configtxlator proto_encode --input config_update_in_envelope.json --type common.Envelope >"${OUTPUT}"
+  set +x
+}

grepruby/hyperlegder-fabric-on-kubernetes

destroy-peers.py
@@ -1,0 +1,34 @@
+#!/usr/bin/env python
+import yaml
+import os
+import sys
+
+print("""\033[93m
+    | |                            
+  __| | __ _ _ __   __ _  ___ _ __ 
+ / _` |/ _` | '_ \ / _` |/ _ \ '__|
+| (_| | (_| | | | | (_| |  __/ |   
+ \__,_|\__,_|_| |_|\__, |\___|_|   
+                    __/ |          
+                   |___/           
+""")
+print("NOTE: This operation will remove the existing fabric peer cluster \033[0m")
+res = raw_input("Do you wish to continue (y/n):")
+if res != 'y':
+  sys.exit(0)
+
+with open("./crypto-config.yaml", 'r') as stream:
+  try:
+    config = yaml.load(stream)
+    os.system('helm del --purge public-certs-pvc')
+    for org in config['PeerOrgs']:
+      namespace = org['Name'].lower()
+      os.system("helm del --purge %s-example-node" %namespace)
+      os.system("helm del --purge cli-%s-pvc" %namespace)
+      os.system("helm del --purge cli-%s" %namespace)
+      for p in org['Specs']:
+        os.system("helm del --purge pvc-%s" %p['CommonName'])
+        os.system("helm del --purge %s" %p['CommonName'])
+      
+  except yaml.YAMLError as exc:
+    print(exc)

org-peer/templates/_helpers.tpl
@@ -1,0 +1,55 @@
+{{/* vim: set filetype=mustache: */}}
+{{/*
+Expand the name of the chart.
+*/}}
+{{- define "example-ecosystem.name" -}}
+{{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+
+{{/*
+Create a default fully qualified app name.
+We truncate at 63 chars because some Kubernetes name fields are limited to this (by the DNS naming spec).
+If release name contains chart name it will be used as a full name.
+*/}}
+{{- define "example-ecosystem.fullname" -}}
+{{- if .Values.fullnameOverride -}}
+{{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" -}}
+{{- else -}}
+{{- $name := default .Chart.Name .Values.nameOverride -}}
+{{- if contains $name .Release.Name -}}
+{{- .Release.Name | trunc 63 | trimSuffix "-" -}}
+{{- else -}}
+{{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+{{- end -}}
+{{- end -}}
+
+{{/*
+Create chart name and version as used by the chart label.
+*/}}
+{{- define "example-ecosystem.chart" -}}
+{{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+
+{{- define "hlf-peer.name" -}}
+{{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+
+{{- define "hlf-peer.fullname" -}}
+{{- if .Values.fullnameOverride -}}
+{{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" -}}
+{{- else -}}
+{{- $name := default .Chart.Name .Values.nameOverride -}}
+{{- if contains $name .Release.Name -}}
+{{- .Release.Name | trunc 63 | trimSuffix "-" -}}
+{{- else -}}
+{{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" -}}
+{{- end -}}
+{{- end -}}
+{{- end -}}
+{{- define "labels.standard" -}}
+app: {{ include "hlf-peer.name" . }}
+heritage: {{ .Release.Service | quote }}
+release: {{ .Release.Name | quote }}
+chart: {{ include "hlf-peer.chart" . }}
+{{- end -}}

scripts/script.sh
@@ -1,0 +1,117 @@
+#!/bin/bash
+
+echo
+echo " ____    _____      _      ____    _____ "
+echo "/ ___|  |_   _|    / \    |  _ \  |_   _|"
+echo "\___ \    | |     / _ \   | |_) |   | |  "
+echo " ___) |   | |    / ___ \  |  _ <    | |  "
+echo "|____/    |_|   /_/   \_\ |_| \_\   |_|  "
+echo
+echo
+CHANNEL_NAME="$1"
+DELAY="$2"
+LANGUAGE="$3"
+TIMEOUT="$4"
+VERBOSE="$5"
+: ${CHANNEL_NAME:="mychannel"}
+: ${DELAY:="3"}
+: ${LANGUAGE:="golang"}
+: ${TIMEOUT:="10"}
+: ${VERBOSE:="false"}
+LANGUAGE=`echo "$LANGUAGE" | tr [:upper:] [:lower:]`
+COUNTER=1
+MAX_RETRY=5
+
+
+# CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/sla_chaincode/node/"
+CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/default_chaincode/node/"
+
+echo "Channel name : "$CHANNEL_NAME
+
+# import utils
+. scripts/utils.sh
+
+createChannel() {
+	setGlobals 0 1
+
+	if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+                set -x
+		peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx >&log.txt
+		res=$?
+                set +x
+	else
+				set -x
+		peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA >&log.txt
+		res=$?
+				set +x
+	fi
+	cat log.txt
+	verifyResult $res "Channel creation failed"
+	echo "===================== Channel '$CHANNEL_NAME' created ===================== "
+	echo
+}
+
+joinChannel () {
+	for org in 1 2; do
+	    for peer in 0 1; do
+		joinChannelWithRetry $peer $org
+		echo "===================== peer${peer}.org${org} joined channel '$CHANNEL_NAME' ===================== "
+		sleep $DELAY
+		echo
+	    done
+	done
+}
+
+## Create channel
+echo "Creating channel..."
+createChannel
+
+## Join all the peers to the channel
+echo "Having all peers join the channel..."
+joinChannel
+
+## Set the anchor peers for each org in the channel
+echo "Updating anchor peers for org1..."
+updateAnchorPeers 0 1
+echo "Updating anchor peers for org2..."
+updateAnchorPeers 0 2
+
+## Install chaincode on peer0.org1 and peer0.org2
+echo "Installing chaincode on peer0.org1..."
+installChaincode 0 1
+echo "Install chaincode on peer0.org2..."
+installChaincode 0 2
+
+# Instantiate chaincode on peer0.org2
+echo "Instantiating chaincode on peer0.org2..."
+instantiateChaincode 0 2
+
+# Query chaincode on peer0.org1
+# echo "Querying chaincode on peer0.org1..."
+# chaincodeQuery 0 1 100
+
+# Invoke chaincode on peer0.org1 and peer0.org2
+# echo "Sending invoke transaction on peer0.org1 peer0.org2..."
+# chaincodeInvoke 0 1 0 2
+
+## Install chaincode on peer1.org2
+echo "Installing chaincode on peer1.org2..."
+installChaincode 1 2
+
+# Query on chaincode on peer1.org2, check if the result is 90
+# echo "Querying chaincode on peer1.org2..."
+# chaincodeQuery 1 2 90
+
+echo
+echo "========= All GOOD, execution completed =========== "
+echo
+
+echo
+echo " _____   _   _   ____   "
+echo "| ____| | \ | | |  _ \  "
+echo "|  _|   |  \| | | | | | "
+echo "| |___  | |\  | | |_| | "
+echo "|_____| |_| \_| |____/  "
+echo
+
+exit 0

scripts/utils.sh
@@ -1,0 +1,194 @@
+ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+PEER0_ORG1_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/buyerone.example.com/peers/peer0.buyerone.example.com/tls/ca.crt
+PEER0_ORG2_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/sellerone.example.com/peers/peer0.sellerone.example.com/tls/ca.crt
+
+# verify the result of the end-to-end test
+verifyResult() {
+  if [ $1 -ne 0 ]; then
+    echo "!!!!!!!!!!!!!!! "$2" !!!!!!!!!!!!!!!!"
+    echo "========= ERROR !!! FAILED to execute End-2-End Scenario ==========="
+    echo
+    exit 1
+  fi
+}
+
+# Set OrdererOrg.Admin globals
+setOrdererGlobals() {
+  CORE_PEER_LOCALMSPID="OrdererMSP"
+  CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+  CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/users/Admin@example.com/msp
+}
+
+setGlobals() {
+  PEER=$1
+  ORG=$2
+  if [ $ORG -eq 1 ]; then
+    CORE_PEER_LOCALMSPID="BuyerOneMSP"
+    CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+    CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/buyerone.example.com/users/Admin@buyerone.example.com/msp
+    if [ $PEER -eq 0 ]; then
+      CORE_PEER_ADDRESS=peer0.buyerone.example.com:7051
+    else
+      CORE_PEER_ADDRESS=peer1.buyerone.example.com:7051
+    fi
+  elif [ $ORG -eq 2 ]; then
+    CORE_PEER_LOCALMSPID="SellerOneMSP"
+    CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
+    CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/sellerone.example.com/users/Admin@sellerone.example.com/msp
+    if [ $PEER -eq 0 ]; then
+      CORE_PEER_ADDRESS=peer0.sellerone.example.com:7051
+    else
+      CORE_PEER_ADDRESS=peer1.sellerone.example.com:7051
+    fi
+  else
+    echo "================== ERROR !!! ORG Unknown =================="
+  fi
+
+  if [ "$VERBOSE" == "true" ]; then
+    env | grep CORE
+  fi
+}
+
+updateAnchorPeers() {
+  PEER=$1
+  ORG=$2
+  setGlobals $PEER $ORG
+
+  if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+    set -x
+    peer channel update -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx >&log.txt
+    res=$?
+    set +x
+  else
+    set -x
+    peer channel update -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA >&log.txt
+    res=$?
+    set +x
+  fi
+  cat log.txt
+  verifyResult $res "Anchor peer update failed"
+  echo "===================== Anchor peers updated for org '$CORE_PEER_LOCALMSPID' on channel '$CHANNEL_NAME' ===================== "
+  sleep $DELAY
+  echo
+}
+
+## Sometimes Join takes time hence RETRY at least 5 times
+joinChannelWithRetry() {
+  PEER=$1
+  ORG=$2
+  setGlobals $PEER $ORG
+
+  set -x
+  peer channel join -b $CHANNEL_NAME.block >&log.txt
+  res=$?
+  set +x
+  cat log.txt
+  if [ $res -ne 0 -a $COUNTER -lt $MAX_RETRY ]; then
+    COUNTER=$(expr $COUNTER + 1)
+    echo "peer${PEER}.org${ORG} failed to join the channel, Retry after $DELAY seconds"
+    sleep $DELAY
+    joinChannelWithRetry $PEER $ORG
+  else
+    COUNTER=1
+  fi
+  verifyResult $res "After $MAX_RETRY attempts, peer${PEER}.org${ORG} has failed to join channel '$CHANNEL_NAME' "
+}
+
+installChaincode() {
+  PEER=$1
+  ORG=$2
+  setGlobals $PEER $ORG
+  VERSION=${3:-1.0}
+  set -x
+  peer chaincode install -n mycc -v ${VERSION} -l ${LANGUAGE} -p ${CC_SRC_PATH} >&log.txt
+  res=$?
+  set +x
+  cat log.txt
+  verifyResult $res "Chaincode installation on peer${PEER}.org${ORG} has failed"
+  echo "===================== Chaincode is installed on peer${PEER}.org${ORG} ===================== "
+  echo
+}
+
+instantiateChaincode() {
+  PEER=$1
+  ORG=$2
+  setGlobals $PEER $ORG
+  VERSION=${3:-1.0}
+
+  # while 'peer chaincode' command can get the orderer endpoint from the peer
+  # (if join was successful), let's supply it directly as we know it using
+  # the "-o" option
+  if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+    set -x
+    peer chaincode instantiate -o orderer.example.com:7050 -C $CHANNEL_NAME -n mycc -l ${LANGUAGE} -v ${VERSION} -c '{"Args":["init","key","100"]}' -P "OR ('BuyerOneMSP.peer','SellerOneMSP.peer')" >&log.txt
+    res=$?
+    set +x
+  else
+    set -x
+    # peer chaincode instantiate -o orderer.example.com:7050 --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -C $CHANNEL_NAME -n mycc -l ${LANGUAGE} -v 1.0 -c '{"Args":["init", "{\"locations\":{\"longitude\":\"10.10\",\"latitude\":\"10.10\",\"altitude\":\"10.10\"},\"technology\":\"LTE\",\"technologyOptions\":\"Rel 13\",\"qualityOfService\":{\"totalAvailableBandwidth\":\"10\",\"peakUplinkSpeed\":\"20\"},\"accessControlType\":\"PLMN\",\"userIdentity\":\"user code of buyers users\",\"routingType\":\"LB\",\"metricOfValue\":{\"IMSI\":\"91\"},\"timePeriod\":{\"from\":\"some epoch time\",\"to\":\"some other epoch time > from\"},\"price\":{\"perGB\":\"10\"},\"pricingType\":\"one time\",\"recurringFrequency\":\"daily\",\"onetimeDuratrion\":\"1 year\",\"paymentTerms\":\"up-front\",\"prepaidAmount\":\"100\",\"topup amount\":\"100\",\"renewals\":\"5 days\",\"contractSeller\":\"seller ID\",\"contractBuyer\":\"buyer ID\",\"contractStatus\":\"inactive\",\"performanceReporting\":[\"number of unique users\",\"square feet utilized\"],\"reportingTolerance\":\"10\",\"reportingFrequency\":\"monthly\"}"]}' -P "OR ('BuyerOneMSP.peer','SellerOneMSP.peer')" >&log.txt
+    # 
+    # 
+    peer chaincode instantiate -o orderer.example.com:7050 --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA -C $CHANNEL_NAME -n mycc -l ${LANGUAGE} -v 1.0 -c '{"Args":["init","key","jayesh"]}' -P "OR ('BuyerOneMSP.peer','SellerOneMSP.peer')" >&log.txt
+    res=$?
+    set +x
+  fi
+  cat log.txt
+  verifyResult $res "Chaincode instantiation on peer${PEER}.org${ORG} on channel '$CHANNEL_NAME' failed"
+  echo "===================== Chaincode is instantiated on peer${PEER}.org${ORG} on channel '$CHANNEL_NAME' ===================== "
+  echo
+}
+
+# fetchChannelConfig <channel_id> <output_json>
+# Writes the current channel config for a given channel to a JSON file
+fetchChannelConfig() {
+  CHANNEL=$1
+  OUTPUT=$2
+
+  setOrdererGlobals
+
+  echo "Fetching the most recent configuration block for the channel"
+  if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+    set -x
+    peer channel fetch config config_block.pb -o orderer.example.com:7050 -c $CHANNEL --cafile $ORDERER_CA
+    set +x
+  else
+    set -x
+    peer channel fetch config config_block.pb -o orderer.example.com:7050 -c $CHANNEL --tls --cafile $ORDERER_CA
+    set +x
+  fi
+
+  echo "Decoding config block to JSON and isolating config to ${OUTPUT}"
+  set -x
+  configtxlator proto_decode --input config_block.pb --type common.Block | jq .data.data[0].payload.data.config >"${OUTPUT}"
+  set +x
+}
+
+# signConfigtxAsPeerOrg <org> <configtx.pb>
+# Set the peerOrg admin of an org and signing the config update
+signConfigtxAsPeerOrg() {
+  PEERORG=$1
+  TX=$2
+  setGlobals 0 $PEERORG
+  set -x
+  peer channel signconfigtx -f "${TX}"
+  set +x
+}
+
+# createConfigUpdate <channel_id> <original_config.json> <modified_config.json> <output.pb>
+# Takes an original and modified config, and produces the config update tx
+# which transitions between the two
+createConfigUpdate() {
+  CHANNEL=$1
+  ORIGINAL=$2
+  MODIFIED=$3
+  OUTPUT=$4
+
+  set -x
+  configtxlator proto_encode --input "${ORIGINAL}" --type common.Config >original_config.pb
+  configtxlator proto_encode --input "${MODIFIED}" --type common.Config >modified_config.pb
+  configtxlator compute_update --channel_id "${CHANNEL}" --original original_config.pb --updated modified_config.pb >config_update.pb
+  configtxlator proto_decode --input config_update.pb --type common.ConfigUpdate >config_update.json
+  echo '{"payload":{"header":{"channel_header":{"channel_id":"'$CHANNEL'", "type":2}},"data":{"config_update":'$(cat config_update.json)'}}}' | jq . >config_update_in_envelope.json
+  configtxlator proto_encode --input config_update_in_envelope.json --type common.Envelope >"${OUTPUT}"
+  set +x
+}